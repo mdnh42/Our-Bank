44f7d5ed9abdbfda24e1a675c9c952d6152c3f40
mdnh42 <mdnh.42@gmail.com>
2021-08-18 00:53:16 +0600

    update

--- script/script.js
+++ script/script.js
@@ -1,18 +1,53 @@
- //Create add event listner 
- document.getElementById('login-submit').addEventListener('click', function(){
-    // call email input
-    const emailFied = document.getElementById('user-email').value;
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const amountValue = parseFloat(inputAmountText);
     
-    // call pass input
-    const passFied = document.getElementById('user-password').value;
-    
-    if(emailFied == 'admin' && passFied == '123'){
-        window.location.href='banking.html';
-    }
-    else{
-        alert('You ID & Pass Wrong. Please type: admin & 123');
-    }
-    
-})
+    inputField.value = '';
+    return amountValue;
+}
 
+function updateTotalField(totalFieldId, amount) {
+    const totalElement = document.getElementById(totalFieldId);
+    const totalText = totalElement.innerText;
+    const previousTotal = parseFloat(totalText);
+    totalElement.innerText = previousTotal + amount;
+}
 
+function getCurrentBalance() {
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    return previousBalanceTotal;
+}
+
+function updateBalance(amount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = getCurrentBalance();
+    if (isAdd == true) {
+        balanceTotal.innerText = previousBalanceTotal + amount;
+    }
+    else {
+        balanceTotal.innerText = previousBalanceTotal - amount;
+    }
+}
+
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+        updateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
+});
+
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    const withdrawAmount = getInputValue('withdraw-input');
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+        updateTotalField('withdraw-total', withdrawAmount);
+        updateBalance(withdrawAmount, false);
+    }
+    if (withdrawAmount > currentBalance) {
+        console.log('You can not withdraw more than what you have in your account');
+    }
+});
